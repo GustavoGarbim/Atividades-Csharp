446f4d91bdc82406b263cd0a454e50ab639d1919
GUSTAVO PEREIRA GARBIM <gustavogarbim850@gmail.com>
2026-02-20 15:00:22 -0300

    Estrutura de Dados

---
+++ Exercicios/Estrutura1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    internal class Estrutura1
+    {
+        public static void SomaElementos(int[] valores)
+        {
+            var proximo = 0;
+            foreach (int atual in valores)
+            {
+                proximo += atual;
+            }
+            Console.WriteLine($"Total: {proximo}");
+        }
+    }
+}
---
+++ Exercicios/Estrutura2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    internal class Estrutura2
+    {
+        public static void MaiorElemento(double[] fileira)
+        {
+            var maior = fileira[0];
+            foreach (double atual in fileira)
+            {
+                if (atual > maior)
+                {
+                    maior = atual;
+                }
+            }
+            Console.WriteLine(maior);
+        }
+    }
+}
