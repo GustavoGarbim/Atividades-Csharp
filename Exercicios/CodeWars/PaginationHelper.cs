19a63e40dba07d0c2244acba7b9bd7b534c86667
GUSTAVO PEREIRA GARBIM <gustavogarbim850@gmail.com>
2026-03-17 14:56:38 -0300

    Add new CodeWars: PaginationHelper 5 kyu

---
+++ Exercicios/CodeWars/PaginationHelper.cs
@@ -0,0 +1,53 @@
+﻿namespace Exercicios.CodeWars
+{
+    public class PaginationHelper<T>
+    {
+        private IList<T> _collection;
+        private int _itemsPerPage;
+
+        public PaginationHelper(IList<T> collection, int itemsPerPage)
+        {
+            _collection = collection;
+            _itemsPerPage = itemsPerPage;
+        }
+
+        public int ItemCount
+        {
+            get { return _collection.Count; }
+        }
+
+        public int PageCount
+        {
+            get
+            {
+                return (int)Math.Ceiling((double)_collection.Count / _itemsPerPage);
+            }
+        }
+
+        public int PageItemCount(int pageIndex)
+        {
+            if (pageIndex < 0 || pageIndex >= PageCount)
+            {
+                return -1;
+            }
+
+            if (pageIndex == PageCount - 1)
+            {
+                int resto = _collection.Count % _itemsPerPage;
+                return resto == 0 ? _itemsPerPage : resto;
+            }
+
+            return _itemsPerPage;
+        }
+
+        public int PageIndex(int itemIndex)
+        {
+            if (itemIndex < 0 || itemIndex >= _collection.Count || _collection.Count == 0)
+            {
+                return -1;
+            }
+
+            return itemIndex / _itemsPerPage;
+        }
+    }
+}
