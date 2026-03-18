c8c58eac6abb7026b4a7b9d61c3c4bc309fda9c3
GUSTAVO PEREIRA GARBIM <gustavogarbim850@gmail.com>
2026-03-18 14:58:22 -0300

    Add new CodeWars: SumOfParts 6 kyu

---
+++ Exercicios/CodeWars/SumOfParts.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercicios.CodeWars
+{
+
+    /*
+     * Desafio: Sum of Parts
+     * Lógica de otimização (Complexidade de tempo O(N)):
+     * Para evitar o recálculo repetitivo que causaria lentidão em listas grandes,
+     * a solução utiliza matemática simples:
+     * 1. Calcula a soma total do array apenas uma vez.
+     * 2. Guarda essa soma total como o primeiro item do resultado.
+     * 3. Percorre o array subtraindo o valor do item atual da soma anterior.
+     * 4. Converte a lista dinâmica resultante de volta para um array fixo no retorno.
+     */
+
+    public class SumOfParts
+    {
+        public static int[] PartsSums(int[] ls)
+        {
+            List<int> listasoma = new();
+            var soma = 0;
+            foreach (int i in ls)
+            {
+                soma += i;
+            }
+
+            listasoma.Add(soma);
+
+            foreach (int i in ls)
+            {
+                soma -= i;
+                listasoma.Add(soma);
+            }
+
+            return listasoma.ToArray();
+        }
+    }
+}
