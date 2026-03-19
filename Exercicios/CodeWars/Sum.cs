d5b16efd3185b4bf57d08410f1f2a9ffe64ba649
GUSTAVO PEREIRA GARBIM <gustavogarbim850@gmail.com>
2026-03-19 13:41:02 -0300

    Add new CodeWars: SomOfTwoNumbers 4 kyu!

---
+++ Exercicios/CodeWars/Sum.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace Exercicios.CodeWars
+{
+
+    /*
+        Você deve criar uma função que converta duas strings para numeros e faça a soma deles.
+        nivel 4 kyu.
+     */
+
+    internal class Sum
+    {
+        public static string Add(string a, string b)
+        {
+            BigInteger a1 = BigInteger.Parse(a);
+            BigInteger b1 = BigInteger.Parse(b);
+
+            return (a1 + b1).ToString();
+        }
+    }
+}
--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-
+            string a = "123456789";
+            string b = "987654322";
         }
     }
 }
