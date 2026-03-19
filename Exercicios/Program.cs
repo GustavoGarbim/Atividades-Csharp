e349cfd2809f19277c3e95b07f850e43c9f536ad
GUSTAVO PEREIRA GARBIM <gustavogarbim850@gmail.com>
2026-03-19 15:03:09 -0300

    new exercise

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
+            int str = 1;
+            string str2 = "MoviesAndBooks";
 
+            Console.WriteLine(Kata.ToUnderscore(str2));
+        }
+
+        public static class Kata
+        {
+            public static string ToUnderscore(int str)
+            {
+                throw new NotImplementedException();
+            }
+
+            public static string ToUnderscore(string str)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
